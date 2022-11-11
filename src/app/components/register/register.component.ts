539cd7ba3406bb40aa3247422fae4f6a42f99575
Farid ullah <faridullah996@gmail.com>
2022-11-11 22:19:42 +0500

    register working

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatStepper } from '@angular/material/stepper';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,32 +9,49 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
-	theForm: FormGroup;
+export class RegisterComponent {
+	basicInfoForm: FormGroup;
+	credentialsForm: FormGroup;
+	terms = new FormControl(null, [Validators.required]);
 	
 	constructor(private fb: FormBuilder, 
 				private authService: AuthService) 
 	{
-		this.theForm = fb.group({
+		this.basicInfoForm = fb.group({
 			firstName: ['', [Validators.required]],
 			lastName: ['', [Validators.required]],
 			cellNumber: ['', [Validators.required]],
 			idNumber: ['', [Validators.required]],
-			email: ['', [Validators.required]],
-			
-			terms: [false, [Validators.required]],
+			email: ['', [Validators.required, Validators.email]],
+		});
 
+		this.credentialsForm = fb.group({
 			userName: ['', [Validators.required]],
 			password: ['', [Validators.required]],
 			confirmPassword: ['', [Validators.required]]
 		});
 	}
 
-	ngOnInit(): void {
-	}
+	numericOnly(ev: any): boolean
+    {
+		const allowedKeys = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Tab'];
+		if (allowedKeys.includes(ev.key)) return true;
+
+        const letters = /^[0-9]+$/;
+        if (ev.key && ev.key.match(letters))
+        {
+            return (ev.key.match(letters).length > 0);
+        }
+
+        return false;
+    }
 
 	onSubmit(stepper: MatStepper): void {
-		const payload = this.theForm.value;
+		const payload = {
+			basic_info: this.basicInfoForm.value,
+			credentials: this.credentialsForm.value,
+			terms_acceptance: this.terms.value
+		}
 
 		this.authService.register(payload).subscribe({
 			next: () => { stepper.next(); },
