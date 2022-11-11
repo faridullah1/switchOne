6c3391ce8087e6e7275193b18fdee2ab4431a456
Farid ullah <faridullah996@gmail.com>
2022-11-11 21:36:12 +0500

    landing page content and styling

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Card, Tab } from 'src/app/models';
+
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+	tabs: Tab[] = [];
+	cards: Card[] = [];
+	
+	constructor() {
+		this.tabs = [
+			{ title: 'Features', selected: true },
+			{ title: 'How to use', selected: false },
+			{ title: 'Security', selected: false },
+			{ title: 'Pay and buy', selected: false },
+		];
 
-  constructor() { }
+		this.cards = [
+			{ title: 'Pay Bills & Account', description: 'Some description', actionText: 'Bills and Accounts'},
+			{ title: 'Pay Bills & Account', description: 'Some description', actionText: 'Bills and Accounts'},
+			{ title: 'Pay Bills & Account', description: 'Some description', actionText: 'Bills and Accounts'}
+		]
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	onTabChange(tab: Tab): void {
+		for (let t of this.tabs) {
+			t.selected = false;
+		}
+
+		tab.selected = true;
+	}
 }
---
+++ src/app/models.ts
@@ -0,0 +1,10 @@
+export interface Tab {
+	title: string;
+	selected: boolean
+}
+
+export interface Card {
+	title: string;
+	description: string;
+	actionText: string;
+}
