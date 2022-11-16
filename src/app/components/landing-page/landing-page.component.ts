c5c7168fb96dffa8634b8781eeebb03218cdbe4d
Farid ullah <faridullah996@gmail.com>
2022-11-16 08:12:47 +0500

    features and how_to_use shifted to components

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Card, Tab } from 'src/app/models';
+import { Component } from '@angular/core';
+import { Tab } from 'src/app/models';
 
 
 @Component({
@@ -7,9 +7,8 @@ import { Card, Tab } from 'src/app/models';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent {
 	tabs: Tab[] = [];
-	cards: Card[] = [];
 	selectedTab: Tab;
 	
 	constructor() {
@@ -20,16 +19,7 @@ export class LandingPageComponent implements OnInit {
 			{ name: 'billing', title: 'Pay and buy', selected: false },
 		];
 
-		this.cards = [
-			{ title: 'Pay Bills & Account', description: 'pay for your bills and account, including TV license and DSTV, By simple using the refrence Number found on your bills, Invoice or account statement', actionText: 'Bills and Accounts'},
-			{ title: 'Pre Paid Electicity, Water or Gas', description: 'Buy pre paid Electricity or gas by simple adding your meter number and choosing the amount you would like to purchase', actionText: 'Prepaid'},
-			{ title: 'Pay Trafic Fine', description: 'Search and pay for trafic fine Using your ID number or trafic fine refrence number', actionText: 'Fines'}
-		];
-
 		this.selectedTab = this.tabs[0];
-	}
-
-	ngOnInit(): void {
 	}
 
 	onTabChange(tab: Tab): void {
